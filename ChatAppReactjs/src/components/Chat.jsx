f1a0e555ea7aa1ead4daf327dc9aa619d00ca123
Abir Hasan Al-amin <hasan.alamin.abir+gitHUB@gmail.com>
2023-10-28 22:04:08 +0600

    upload

--- ChatAppReactjs/src/components/Chat.jsx
+++ ChatAppReactjs/src/components/Chat.jsx
@@ -1,31 +1,67 @@
 import React, { useEffect, useState } from "react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { auth, db } from "../config/firebase";
-import { getDocs, collection } from "firebase/firestore";
+import { getDocs, collection, getDoc, setDoc, doc } from "firebase/firestore";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 function Chat() {
   const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(null);
   const userCollection = collection(db, "users");
-  const currentUser = auth.currentUser;
   const navigate = useNavigate();
   useEffect(() => {
-    const getUsers = async () => {
-      try {
-        const data = await getDocs(userCollection);
-        const filterData = data.docs
-          .map((doc) => ({
-            ...doc.data(),
-            id: doc.id,
-          }))
-          .filter((user) => user.id !== currentUser.uid);
-        setUsers(filterData);
-      } catch (error) {
-        console.error(error);
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
       }
-    };
-    getUsers();
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleConversationStart = async (selectedUser) => {
+    try {
+      if (currentUser) {
+        const participants = [currentUser.uid, selectedUser.id];
+        participants.sort();
+        const conversationID = participants.join("-");
+        navigate(`/Msg`, {
+          state: {
+            conversationID: conversationID,
+            name: selectedUser.Name,
+            photo: selectedUser.Profile,
+            currentUserID: currentUser.uid,
+          },
+        });
+      }
+    } catch (error) {
+      console.error("Error starting conversation:", error);
+    }
+  };
+
+  const getUsers = async () => {
+    try {
+      const data = await getDocs(userCollection);
+      const filterData = data.docs
+        .map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        }))
+        .filter((user) => user.id !== currentUser?.uid);
+      setUsers(filterData);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (currentUser) {
+      getUsers();
+    }
   }, [currentUser]);
+
   const logout = () => {
     signOut(auth)
       .then(() => {
@@ -35,6 +71,10 @@ function Chat() {
         console.error(error);
       });
   };
+
+  if (!currentUser) {
+    return <div>Loading...</div>;
+  }
   return (
     <div className="h-full w-full lg:w-[700px] lg:h-[90%] lg:rounded-xl bg-[#ABBCEA] flex flex-col">
       <div className="flex justify-between p-4 my-5 lg:p-3">
@@ -48,9 +88,18 @@ function Chat() {
           {users.map((user) => (
             <li
               key={user.id}
+              onClick={() => {
+                handleConversationStart(user);
+              }}
               className="flex items-center gap-3 p-3 m-3 bg-[#CBF1F5] rounded-3xl"
             >
-              <img src={user.Profile} className=" w-12 h-12 rounded-full" />
+              <img
+                src={
+                  user.Profile ||
+                  "https://static.vecteezy.com/system/resources/previews/020/765/399/non_2x/default-profile-account-unknown-icon-black-silhouette-free-vector.jpg"
+                }
+                className=" w-12 h-12 rounded-full"
+              />
               <p className="font-medium md:text-xl">{user.Name}</p>
             </li>
           ))}
--- ChatAppReactjs/src/components/Msg.jsx
+++ ChatAppReactjs/src/components/Msg.jsx
@@ -1,23 +1,86 @@
-import React from 'react'
-import {BsUpload,BsSend} from 'react-icons/bs'
-import dog from '../assets/dog1.jpg'
+import { addDoc, collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { BsUpload, BsSend } from "react-icons/bs";
+import { useLocation } from "react-router-dom";
+import { db } from "../config/firebase";
 function Msg() {
-    return (
-        <div className='h-full w-full lg:w-[700px] lg:h-[90%] lg:rounded-xl bg-[#ABBCEA] flex flex-col'>
-        <div className='flex p-4 my-5 lg:p-3 gap-5'>
-            <img src={dog} className='md:ml-10 w-12 h-12 rounded-full'/>
-            <p className=' font-medium text-3xl md:text-4xl '>Dog</p>
+  const location = useLocation();
+  const { conversationID, name, photo, currentUserID } = location.state || {};
+  const [newMessage, setNewMessage] = useState("");
+  const [messages, setMessages] = useState("");
+  const fetchMessages = async () => {
+    try {
+      const conversationRef = collection(
+        db,
+        "conversations",
+        conversationID,
+        "messages"
+      );
+      const querySnapshot = await getDocs(conversationRef);
+      let data = [];
+      querySnapshot.forEach((doc) => {
+        data.push({ id: doc.id, ...doc.data() });
+      });
+      setMessages(data);
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  };
+  useEffect(() => {
+    fetchMessages();
+  }, [conversationID]);
+
+  const handleSendMessage = async () => {
+    if (newMessage.trim() !== "") {
+      try {
+        const conversationRef = collection(db, "conversations", conversationID, "messages");
+        await addDoc(conversationRef, {
+          uid: currentUserID,
+          msg: newMessage,
+          timestamp: new Date().getTime(),
+        });
+        setNewMessage("");
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
+    }
+  };
+  return (
+    <div className="h-full w-full lg:w-[700px] lg:h-[90%] lg:rounded-xl bg-[#ABBCEA] flex flex-col">
+      <div className="flex p-4 my-5 lg:p-3 gap-5">
+        <img src={photo} className="md:ml-10 w-12 h-12 rounded-full" />
+        <p className=" font-medium text-3xl md:text-4xl ">{name}</p>
+      </div>
+      <div className="flex-1 rounded-t-xl  lg:rounded-xl flex flex-col">
+        <div className=" bg-[#FFFFFF] h-20 flex-1 rounded-t-xl">
+          {messages.length > 0 ? (
+            <ul className="p-3">
+              {messages.map((msg) => (
+                <li
+                  key={msg.id}
+                  className="flex items-center w-min gap-3 p-3 m-3 bg-[#CBF1F5] rounded-2xl"
+                >
+                  <p className="font-medium md:text-xl">{msg.msg}</p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p></p>
+          )}
         </div>
-        <div className='flex-1 rounded-t-xl  lg:rounded-xl flex flex-col'>
-            <div className=' bg-[#FFFFFF] h-20 flex-1 rounded-t-xl'></div>
-            <div className=' flex items-center gap-4 p-3 bg-[#8EACCD]  lg:rounded-b-xl justify-around'>
-                <button><BsUpload className='w-6 h-6 lg:w-8 lg:h-8'/></button>
-                <input className=' font-medium md:text-xl w-[80%] h-10 rounded-full outline-none p-3 shadow-sm'/>
-                <button><BsSend className='w-6 h-6 lg:w-8 lg:h-8'/></button>
-            </div>
+        <div className=" flex items-center p-3 bg-[#8EACCD]  lg:rounded-b-xl justify-around">
+          <input
+            className="font-medium md:text-xl w-[80%] h-10 rounded-full outline-none p-3 shadow-sm"
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button onClick={handleSendMessage}>
+            <BsSend className="w-6 h-6 lg:w-8 lg:h-8" />
+          </button>
         </div>
-        </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Msg
+export default Msg;
